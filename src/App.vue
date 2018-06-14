973f3d3b2c1b78467a69fb5706887c8a31e266cc
xchener <pfhcxzzaq@163.com>
2018-06-14 18:22:16 +0800

    01：搭架子

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <!-- <div class="header">
+      I am header！
+    </div> -->
+    <sell-header></sell-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content">
+      I am content!
+    </div>
+  </div>
+</template>
+
+<script>
+import SellHeader from './components/header/SellHeader.vue';
+export default {
+  components: {
+    SellHeader
+  }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+#app
+  .tab
+    width 100%
+    display flex
+    height 40px
+    line-height 40px
+    .tab-item
+      flex 1
+      text-align center
+</style>
---
+++ src/components/header/SellHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="Vheader">
+    我是header!
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'Vheader'
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
